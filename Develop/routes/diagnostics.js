8372c7ecd80becbe2ecd54a77ea480a0cb9c52ad
Chrissie Sparling <gigglewrks@aol.com>
2022-01-28 14:50:55 -0800

    updates to dev folder

---
+++ Develop/routes/diagnostics.js
@@ -0,0 +1,34 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving diagnostic information
+diagnostics.get('/', (req, res) => {
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)));
+  //Logic for sending all the content of db/diagnostics.json
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { errors } = req.body;
+  const time = Date.now();
+
+  if (req.body) {
+    const newDiag = {
+      time,
+      errors,
+      error_id: uuidv4(),
+    };
+
+    readAndAppend(newDiag, './db/diagnostics.json');
+    res.json('Diagnostic added successfully');
+  } else {
+    res.error('Error in adding diagnostic')
+  }
+});
+  //  Logic for appending data to the db/diagnostics.json file
+
+module.exports = diagnostics;
+  
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # express-stuff
+right click on server.js
+open in terminal
+in terminal run:
+npm init
+npm i 
+nodemon server.js
+
+then click on the link provided to see the page at localhost: 3001
+
+follow the instructions
+you should be all set
+
+# link to my git hub / heroku repos
+https://github.com/ChrissieSparling/express-stuff
+
+# snippet
